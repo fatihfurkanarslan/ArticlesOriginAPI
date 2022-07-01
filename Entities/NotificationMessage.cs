1e5e7baba5929ad0e1e0728050206ebc8cfc4cac
fatihfurkanarslan <arslanfatihfurkan@gmail.com>
2022-07-01 15:49:52 +0300

    notifyUser class

---
+++ Entities/NotificationMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class NotificationMessage
+    {
+
+        public int Id { get; set; }
+
+        public string NoteTitle { get; set; }
+
+        public string NoteWriter { get; set; }
+
+
+    }
+}
